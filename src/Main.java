8b6ea4d42eb81af8203b37948994282dadfe7a75
katsuro <sarsendimas@gmail.com>
2024-09-07 12:29:34 +0500

    first commit

---
+++ src/Main.java
@@ -0,0 +1,98 @@
+import java.util.Scanner;
+import javax.swing.JOptionPane;
+public class Main {
+
+    public static void main(String[] args) {
+         Scanner scanner = new Scanner(System.in);
+         System.out.println("Input a number: ");
+         int num1 = scanner.nextInt();
+         for(int i = 0; i < 10; i++) {
+              System.out.println(num1 * (i+1));
+         }
+
+
+
+//         Scanner scanner = new Scanner(System.in);
+//         System.out.println("Input first number: ");
+//         int number1 = scanner.nextInt();
+//         System.out.println("Input second number: ");
+//         int number2 = scanner.nextInt();
+//         System.out.println(number1 + "+" + number2 + "=" + (number1 + number2));
+//         System.out.println(number1 + "-" + number2 +  "=" + (number1 - number2));
+//         System.out.println(number1 + "*" + number2 + "=" + (number1 * number2));
+//         System.out.println(number1 + "/" + number2 + "=" + (number1 / number2));
+//         System.out.println(number1 + "%" + number2 + "=" + (number1 % number2));
+
+
+//         Scanner scanner = new Scanner(System.in);
+//         System.out.println("Input first number: ");
+//         int result = scanner.nextInt();
+//         System.out.println("Input second number: ");
+//         int result2 = scanner.nextInt();
+//         System.out.println(result*result2);
+
+
+
+//        int a = -5 + 8 * 6;
+//        int b = (55+9) % 9;
+//        int c = 20 + -3 * 5 / 8;
+//        int d = 5 + 15 / 3 * 2 - 8 % 3;
+//
+//        System.out.println(a + "\n" + b + "\n" + c + "\n" + d);
+
+
+//        System.out.println("Hello World");
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("What is your name: ");
+//        String name = scanner.nextLine();
+//        System.out.println("Hello\n" + name);
+//        System.out.println("What is your age: ");
+//        int age = scanner.nextInt();
+//        System.out.println("Your age: " + age);
+
+//        int friend = 10;
+//        friend*=2;
+//        System.out.println(friend);
+
+
+//        String name = JOptionPane.showInputDialog("Enter your name");
+//        JOptionPane.showMessageDialog(null, "Hello " + name);
+
+//        System.out.println("Hello world!");
+//        System.out.println("It's really good!");
+//
+//
+//        int x = 125;
+//        System.out.println(x);
+//
+//        long y = 23234234234L;
+//        System.out.println(y);
+//
+//        byte z = 127;
+//        System.out.println(z);
+//
+//        double v = 3.14;
+//        System.out.println(v);
+//
+//        boolean b = true;
+//        System.out.println(b);
+//
+//        char symbol = '@';
+//        System.out.println(symbol);
+
+//        String a = "ben";
+//        String b1 = "alex";
+//
+//        a=b1;
+//        System.out.println(a);
+
+
+
+        /*
+
+        \n - new line
+        \t - tab
+
+        */
+    }
+}
